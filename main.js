03239b0745a18789566d551e622e38045c083390
finix222 <ifeanyioshaway@gamil.com>
2023-03-06 06:11:27 -0800

    change

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const primaryHeader = document.querySelector(".primary-header ")
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
-
+const burger = document.querySelector(".icon-hamburger")
+const close = document.querySelector(".icon-close")
 navToggle.addEventListener("click", () => {
   primaryNav.hasAttribute("data-visible")
     ? navToggle.setAttribute("aria-expanded", false)
